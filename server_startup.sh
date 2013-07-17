d5c9290be7ddf6db95f769537695fbc2379b2500
Anthony Nystrom <nystrom.anthony@gmail.com>
2013-07-17 15:05:57 -0500

    Update startup for path

--- server_startup.sh
+++ server_startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+PYTHONPATH=$PYTHONPATH:/root
+PYTHONPATH=$PYTHONPATH:/root/refine
+PYTHONPATH=$PYTHONPATH:/root/refine/jobs/refine
+export PYTHONPATH
 python /root/refine/startup.py
 python /root/refine/tools/mapper_watcher.py &
